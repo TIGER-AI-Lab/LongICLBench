53b1d8e8ec8e5fdbd3dc169237b2ef5d7371ee21
ltl3A87 <55973524+ltl3A87@users.noreply.github.com>
2024-04-03 15:52:00 -0400

    Update script_discovery.sh

--- script_discovery.sh
+++ script_discovery.sh
@@ -1,10 +1,11 @@
 model=$1
 cuda=$2
 test_number=$3
+api_key=$4
 
 export CUDA_VISIBLE_DEVICES=${cuda}
-python3 discovery_infer.py --round 1 -m "${model}" --test_number "${test_number}"
-python3 discovery_infer.py --round 2 -m "${model}" --test_number "${test_number}"
-python3 discovery_infer.py --round 3 -m "${model}" --test_number "${test_number}"
-python3 discovery_infer.py --round 4 -m "${model}" --test_number "${test_number}"
-python3 discovery_infer.py --round 5 -m "${model}" --test_number "${test_number}"
+python3 discovery_infer.py --round 1 -m "${model}" --test_number "${test_number}" --api_key "${api_key}"
+python3 discovery_infer.py --round 2 -m "${model}" --test_number "${test_number}" --api_key "${api_key}"
+python3 discovery_infer.py --round 3 -m "${model}" --test_number "${test_number}" --api_key "${api_key}"
+python3 discovery_infer.py --round 4 -m "${model}" --test_number "${test_number}" --api_key "${api_key}"
+python3 discovery_infer.py --round 5 -m "${model}" --test_number "${test_number}" --api_key "${api_key}"
